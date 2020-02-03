eecaa1dfaa58551cdc53b8dd7fe8a42eb878b172
Ernandes Maia <ernandes.maiamq@gmail.com>
2020-02-03 20:03:14 -0300

    C3 : Conexão com o Mongo

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongo = require('mongodb');
 
 const app = express();
+const charts = require('./routes/chart');
+
+var url = "mongodb://localhost:27017/";
+
+mongo.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("test");
+  dbo.collection("PauDosFerrosSIM").find({"SEXO": 1}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
+app.use('/chart', charts);
 
 const port = (3000);
 
